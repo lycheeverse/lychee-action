1ac7f99d32b8b2051f11392726110ec2e8c95bed
Matthias Endler <matthias-endler@gmx.net>
2020-12-14 01:33:45 +0100

    Cleanup and robustness

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,28 +1,25 @@
 #!/bin/bash -l
-set -uo pipefail
+set -euxo pipefail
 
-LYCHEE_OUTPUT_DIR=${LYCHEE_OUTPUT_DIR:="lychee"}
-LYCHEE_OUTPUT_FILENAME=${LYCHEE_OUTPUT_FILENAME:="out.md"}
+LYCHEE_OUT=${LYCHEE_OUT:="lychee/out.md"}
+LYCHEE_TMP="/tmp/lychee/out.md"
 
 # Create temp dir
-mkdir -p /tmp/lychee
+mkdir -p "$(dirname $LYCHEE_TMP)"
 
 # Execute lychee
-lychee "$@" > /tmp/lychee/out 2>&1
+lychee "$@" --output "$LYCHEE_TMP"
 exit_code=$?
 
 # If link errors were found, create a report in the designated directory
 if [ $exit_code -eq 1 ]; then
-    mkdir -p $LYCHEE_OUTPUT_DIR
-    echo -e '### lychee link checker\nErrors were reported while checking the availability of links.\n```' \
-        >$LYCHEE_OUTPUT_DIR/$LYCHEE_OUTPUT_FILENAME
-    cat /tmp/lychee/out >>$LYCHEE_OUTPUT_DIR/$LYCHEE_OUTPUT_FILENAME
-    echo '```' >>$LYCHEE_OUTPUT_DIR/$LYCHEE_OUTPUT_FILENAME
-    echo "Link checker output file: $LYCHEE_OUTPUT_DIR/$LYCHEE_OUTPUT_FILENAME"
+    mkdir -p "$(dirname $LYCHEE_OUT)"
+    echo 'Errors were reported while checking the availability of links:' > $LYCHEE_OUT
+    cat "$LYCHEE_TMP" >> $LYCHEE_OUT
 fi
 
 # Output to console
-cat /tmp/lychee/out
+cat "$LYCHEE_TMP"
 
 # Pass lychee exit code to next step
 echo ::set-output name=exit_code::$exit_code
