bcf5b7f87d3000bdfee504df3aacab56dc41839b
Matthias <matthias-endler@gmx.net>
2021-02-15 11:02:49 +0100

    Add link to Github Actions output

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@ set -uxo pipefail
 
 LYCHEE_OUT=${LYCHEE_OUT:="lychee/out.md"}
 LYCHEE_TMP="/tmp/lychee/out.md"
+GITHUB_WORKFLOW_URL="https://github.com/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}"
 
 # Create temp dir
 mkdir -p "$(dirname $LYCHEE_TMP)"
@@ -14,8 +15,11 @@ exit_code=$?
 # If link errors were found, create a report in the designated directory
 if [ $exit_code -ne 0 ]; then
     mkdir -p "$(dirname $LYCHEE_OUT)"
-    echo 'Errors were reported while checking the availability of links:' > $LYCHEE_OUT
+    echo 'Errors were reported while checking the availability of links.' > $LYCHEE_OUT
+    echo >> $LYCHEE_OUT
     cat "$LYCHEE_TMP" >> $LYCHEE_OUT
+    echo >> $LYCHEE_OUT
+    echo "[Full Github Actions output](${GITHUB_WORKFLOW_URL})" >> $LYCHEE_OUT
 fi
 
 # Output to console
@@ -23,4 +27,3 @@ cat "$LYCHEE_TMP"
 
 # Pass lychee exit code to next step
 echo ::set-output name=exit_code::$exit_code
-
